ea615c98101fa39fe49a5c32cea0489c40d73280
Jouni Kantola <jouni@kantola.se>
2017-04-07 08:50:14 +0200

    Use ruleset to match chunks

--- plugins/templated-asset-webpack-plugin/src/plugin.js
+++ plugins/templated-asset-webpack-plugin/src/plugin.js
@@ -4,63 +4,31 @@ const fs = require("fs");
 
 const TemplatedChunks = require("./templated-chunks");
 const CompiledChunks = require("./compiled-chunks");
-
-/*new TemplatedAssetWebpackPlugin({
-    chunks: [
-      {
-        name: ['app', 'vendor'] || 'app'
-        test: /lala/,
-        exclude: /(node_modules)/,
-        inline: {
-          template:  path.join(__dirname, 'tmpl/chunk-manifest.tmpl') <-- optional
-          replace: '##SOURCE##'
-        },
-        url: {
-          template:  path.join(__dirname, 'tmpl/chunk-manifest.tmpl')
-          replace: '##URL##'
-          async: true,
-          defer: true
-        }
-      }
-    ]
-  })*/
+const chunkMatcher = require("./chunk-matcher");
 
 class TemplatedAssetWebpackPlugin {
   constructor(opts) {
     const options = opts || {};
-    const templatedChunks = new TemplatedChunks();
+    const templatedChunks = new TemplatedChunks(opts.chunks);
     this.chunks = templatedChunks.chunks;
-
-    // legacy
-    this.sync = options.sync || [];
-    this.async = options.async || [];
-    this.inline = options.inline || [];
-    this.defer = options.defer || [];
-    // this.sync = templatedChunks.sync();
-    // this.async = templatedChunks.async();
-    // this.inline = templatedChunks.inline();
-    // this.defer = templatedChunks.defer();
+    this.sync = templatedChunks.sync();
+    this.async = templatedChunks.async();
+    this.inline = templatedChunks.inline();
+    this.defer = templatedChunks.defer();
   }
 
   apply(compiler) {
     compiler.plugin("emit", (compilation, callback) => {
-      // filter assets to handle
-
       const assets = new CompiledChunks(compilation).chunks;
 
-      const syncAssets = assetPicker(assets, this.sync);
-      const asyncAssets = assetPicker(assets, this.async);
-      const deferredAssets = assetPicker(assets, this.defer);
-      const inlineAssets = assetPicker(assets, this.inline);
+      const syncAssets = chunkMatcher.match(assets, this.sync);
+      const asyncAssets = chunkMatcher.match(assets, this.async);
+      const deferredAssets = chunkMatcher.match(assets, this.defer);
+      const inlineAssets = chunkMatcher.match(assets, this.inline);
 
       const allAssets = syncAssets
         .concat(asyncAssets, deferredAssets, inlineAssets)
         .filter(asset => !!asset);
-
-      // process sync
-      // process async
-      // process inline (manifest.json has specific template)
-      // process defer
 
       const allProcesses = allAssets.map(asset => {
         return asset.process(inline, asset.source).then(template => {
@@ -74,10 +42,6 @@ class TemplatedAssetWebpackPlugin {
       Promise.all(allProcesses).then(_ => callback());
     });
   }
-}
-
-function process(assetHandler, source) {
-  return assetHandler(source);
 }
 
 function inline(source) {
@@ -98,28 +62,6 @@ function inline(source) {
   });
 }
 
-function assetPicker(assets, rules) {
-  return rules.length ? assets.filter(asset => assetFilter(asset, rules)) : [];
-}
-
-function assetFilter(asset, rules) {
-  // name is not enough, does not match manifest.json
-  return rules.map(ruleToRegExp).some(rule => rule.test(asset.name));
-}
-
-function ruleToRegExp(rule) {
-  if (typeof rule === "string") {
-    return new RegExp(`${rule}$`);
-  } else if (rule instanceof RegExp) {
-    return rule;
-  } else if (
-    typeof rule === "object" && !!rule.test && rule.test instanceof RegExp
-  ) {
-    return rule.test;
-  }
-
-  throw `${rule} is not a valid configuration.`;
-}
 function defer(url) {
   return new Promise((resolve, reject) => {
     return readTemplate(
