e9cd8a39cfda4e8e19659d19249d4323746f7fe6
Jouni Kantola <jouni@kantola.se>
2017-04-07 15:28:24 +0200

    Process all rules

--- plugins/templated-asset-webpack-plugin/src/templated-chunks.js
+++ plugins/templated-asset-webpack-plugin/src/templated-chunks.js
@@ -3,49 +3,97 @@
 const chunkMatcher = require("./chunk-matcher");
 const templateReader = require("./template-reader");
 
-// const templatedChunk = {
-//           name: chunk.name,
-//           filename,
-//           source: compilation.assets[filename].source(),
-//           process,
-//           template,
-//           inline
-//           async,
-//           sync,
-//           defer,
-//         };
+class TemplatedChunks {
+  constructor(chunks, rules) {
+    if (!Array.isArray(chunks))
+      throw new TypeError("Chunks must be specified to map chunks with rules.");
+    if (!(typeof rules === "object"))
+      throw new TypeError("Rules must be specified to map rules with chunks.");
 
-function mapSync(chunks, rules) {
-  const syncRules = rules.sync();
-  const syncChunks = chunkMatcher.keep(chunks, syncRules);
+    this.assets = mapChunks(chunks, rules);
+  }
+
+  process(compilation, callback) {
+    const allProcesses = this.assets.map(asset => {
+      return asset.process().then(template => {
+        return new Promise((resolve, reject) => {
+          compilation.assets[asset.name + ".partial"] = template;
+          resolve();
+        });
+      });
+    });
+
+    Promise.all(allProcesses).then(_ => callback());
+  }
+}
+
+function mapChunks(chunks, rules) {
+  const syncChunks = mapSync(chunks, rules);
+  const asyncChunks = mapAsync(chunks, rules);
+  const deferredChunks = mapDeferred(chunks, rules);
+  const inlineChunks = mapInline(chunks, rules);
+
+  const assets = syncChunks
+    .concat(asyncChunks, deferredChunks, inlineChunks)
+    .filter(chunk => !!chunk);
+
+  return assets;
+}
+
+function mapUrl(chunks, rules, template) {
+  /* 
+    const templatedChunk = {
+          name,
+          filename,
+          source,
+          process,
+          template,
+          inline
+          async,
+          sync,
+          defer,
+        };
+*/
+  const syncChunks = chunkMatcher.keep(chunks, rules);
   return syncChunks.map(chunk => {
-    chunk.sync = true; // not needed?
-    const rule = chunkMatcher.match(chunk, syncRules);
+    //chunk.sync = true; chunk.async = true; chunk.defer = true; // not needed?
+    const rule = chunkMatcher.match(chunk, rules);
     // if (rule && rule.template) {
     //   chunk.template = rule.template;
     // } else {
-      chunk.template = __dirname + "/templates/sync.tmpl";
+    chunk.template = __dirname + "/templates/sync.tmpl";
     //}
 
-    chunk.process = _sync.bind(this, chunk.template, chunk.filename);
+    chunk.process = _url.bind(this, chunk.template, chunk.filename);
 
     return chunk;
   });
 }
 
+function mapSync(chunks, rules) {
+  return mapUrl(chunks, rules.sync(), __dirname + "/templates/sync.tmpl");
+}
+
+function mapAsync(chunks, rules) {
+  return mapUrl(chunks, rules.async(), __dirname + "/templates/async.tmpl");
+}
+
+function mapDeferred(chunks, rules) {
+  return mapUrl(chunks, rules.defer(), __dirname + "/templates/defer.tmpl");
+}
+
 function mapInline(chunks, rules) {
   const inlineRules = rules.inline();
-  //console.log("inline rules: ", inlineRules);
   const inlineChunks = chunkMatcher.keep(chunks, inlineRules);
-  //   console.log("inline chunks before map", inlineChunks);
+
   return inlineChunks.map(chunk => {
     chunk.inline = true; // not needed?
     const rule = chunkMatcher.match(chunk, inlineRules);
-    //console.log(rule);
+
     // if (rule && rule.template) {
     //   chunk.template = rule.template;
     // } else {
-      chunk.template = __dirname + "/templates/inline.tmpl";
+    chunk.template = __dirname + "/templates/inline.tmpl";
     //}
 
     chunk.process = _inline.bind(this, chunk.template, chunk.source);
@@ -54,7 +102,7 @@ function mapInline(chunks, rules) {
   });
 }
 
-function _sync(template, url) {
+function _url(template, url) {
   return new Promise((resolve, reject) => {
     return templateReader.read(template).then(content => {
       const script = content.replace(/##URL##/, url);
@@ -82,125 +130,6 @@ function _inline(template, source) {
           return script.length;
         }
       });
-    });
-  });
-}
-
-class TemplatedChunks {
-  constructor(chunks, rules) {
-    if (!Array.isArray(chunks))
-      throw new TypeError("Chunks must be specified to map chunks with rules.");
-    if (!(typeof rules === "object"))
-      throw new TypeError("Rules must be specified to map rules with chunks.");
-
-    const syncChunks = mapSync(chunks, rules);
-    const inlineChunks = mapInline(chunks, rules);
-
-    // this.sync = chunkMatcher.match(assets, this.rules.sync());
-    // this.async = chunkMatcher.match(assets, this.rules.async());
-    // this.defer = chunkMatcher.match(assets, this.rules.defer());
-    // this.inline = chunkMatcher.match(assets, this.rules.inline());
-
-    const assets = syncChunks.concat(inlineChunks).filter(chunk => !!chunk); // deferredChunks, asyncChunks)
-
-    this.assets = assets;
-  }
-
-  process(compilation, callback) {
-    const allProcesses = this.assets.map(asset => {
-      return asset.process().then(template => {
-        return new Promise((resolve, reject) => {
-          compilation.assets[asset.name + ".partial"] = template;
-          resolve();
-        });
-      });
-    });
-
-    Promise.all(allProcesses).then(_ => callback());
-  }
-}
-
-function process(assetHandler, content) {
-  return assetHandler(content);
-}
-
-function inline(source) {
-  return new Promise((resolve, reject) => {
-    return readTemplate(
-      `${__dirname}/../templates/inline.tmpl`
-    ).then(content => {
-      const script = content.replace(/##SOURCE##/, source);
-      resolve({
-        source: function() {
-          return script;
-        },
-        size: function() {
-          return script.length;
-        }
-      });
-    });
-  });
-}
-
-function defer(url) {
-  return new Promise((resolve, reject) => {
-    return readTemplate(
-      `../${__dirname}/../templates/defer.tmpl`
-    ).then(content => {
-      const script = content.replace(/##URL##/, url);
-      resolve({
-        source: function() {
-          return script;
-        },
-        size: function() {
-          return script.length;
-        }
-      });
-    });
-  });
-}
-
-function async(url) {
-  return new Promise((resolve, reject) => {
-    return readTemplate(
-      `../${__dirname}/../templates/async.tmpl`
-    ).then(content => {
-      const script = content.replace(/##URL##/, url);
-      resolve({
-        source: function() {
-          return script;
-        },
-        size: function() {
-          return script.length;
-        }
-      });
-    });
-  });
-}
-
-function sync(url) {
-  return new Promise((resolve, reject) => {
-    return readTemplate(
-      `../${__dirname}/../templates/script.tmpl`
-    ).then(content => {
-      const script = content.replace(/##URL##/, url);
-      resolve({
-        source: function() {
-          return script;
-        },
-        size: function() {
-          return script.length;
-        }
-      });
-    });
-  });
-}
-
-function readTemplate(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, (err, content) => {
-      if (err) reject(err);
-      resolve(content.toString());
     });
   });
 }
