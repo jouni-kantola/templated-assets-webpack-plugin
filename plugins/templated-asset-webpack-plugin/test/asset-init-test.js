56cc6eb22ea31b76808d8fb609a5c51b1072d6db
Jouni Kantola <jouni@kantola.se>
2017-04-09 00:56:01 +0200

    Split tests into multiple files

--- plugins/templated-asset-webpack-plugin/test/asset-init-test.js
+++ plugins/templated-asset-webpack-plugin/test/asset-init-test.js
@@ -2,6 +2,7 @@ import test from "ava";
 import Asset from "../src/asset";
 
 // const a = {
+//   process,
 //   type: {
 //     sync,
 //     async,
@@ -11,8 +12,8 @@ import Asset from "../src/asset";
 //   file: {
 //     prefix,+
 //     extension,+
-//     name,
-//     filename,
+//     name,+
+//     filename,+
 //     content
 //   },
 //   template: {
@@ -30,37 +31,4 @@ test("throw if missing name", t => {
   );
 
   t.is(error.message, "Asset name must be specified");
-});
-
-test("give name", t => {
-  const name = "a-name";
-  const asset = new Asset(name);
-
-  t.is(asset.file.name, name);
-});
-
-test("default prefix", t => {
-  const asset = new Asset("name");
-
-  t.is(asset.file.prefix, "");
-});
-
-test("default extension", t => {
-  const asset = new Asset("name");
-
-  t.is(asset.file.extension, "html");
-});
-
-test("concat filename", t => {
-  const asset = new Asset("name");
-  asset.file.prefix = "_";
-
-  t.is(asset.file.filename, "_name.html");
-});
-
-test("ensure delimited name and extension", t => {
-  const asset = new Asset("name");
-  asset.file.extension = "..23...ext";
-
-  t.is(asset.file.filename, "name.23.ext");
 });
