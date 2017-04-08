d2a4a70a6524fa3f81ae682e1b63d3d0004a29dd
Jouni Kantola <jouni@kantola.se>
2017-04-09 00:54:05 +0200

    Asset file tests

---
+++ plugins/templated-asset-webpack-plugin/test/asset-file-test.js
@@ -0,0 +1,35 @@
+import test from "ava";
+import Asset from "../src/asset";
+
+test("give name", t => {
+  const name = "a-name";
+  const asset = new Asset(name);
+
+  t.is(asset.file.name, name);
+});
+
+test("default prefix", t => {
+  const asset = new Asset("name");
+
+  t.is(asset.file.prefix, "");
+});
+
+test("default extension", t => {
+  const asset = new Asset("name");
+
+  t.is(asset.file.extension, "html");
+});
+
+test("concat filename", t => {
+  const asset = new Asset("name");
+  asset.file.prefix = "_";
+
+  t.is(asset.file.filename, "_name.html");
+});
+
+test("ensure delimited name and extension", t => {
+  const asset = new Asset("name");
+  asset.file.extension = "..23...ext";
+
+  t.is(asset.file.filename, "name.23.ext");
+});
