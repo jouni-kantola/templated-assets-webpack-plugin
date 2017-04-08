f3f9a39625f42dc9cdc155f9da7ee44dcf8c32aa
Jouni Kantola <jouni@kantola.se>
2017-04-09 00:54:33 +0200

    Asset template tests

---
+++ plugins/templated-asset-webpack-plugin/test/asset-template-test.js
@@ -0,0 +1,112 @@
+import test from "ava";
+import Asset from "../src/asset";
+
+test("default template", t => {
+  const asset = new Asset("name");
+
+  t.true(asset.template.path.endsWith("/templates/sync.tmpl"));
+});
+
+test("throw when template path not specified as string", t => {
+  const asset = new Asset("name");
+
+  const error = t.throws(
+    () => {
+      asset.template.path = 1;
+    },
+    Error
+  );
+
+  t.is(error.message, "Specify path to template (as string)");
+});
+
+test("default template is replaced", t => {
+  const asset = new Asset("name");
+  asset.template.path = "a/path";
+
+  t.is(asset.template._path, "a/path");
+  t.is(asset.template.path, "a/path");
+});
+
+test("default sync template", t => {
+  const asset = new Asset("name");
+
+  t.true(asset.type.sync);
+  t.true(asset.template.path.endsWith("/templates/sync.tmpl"));
+});
+
+test("default async/defer template", t => {
+  const asset = new Asset("name");
+  asset.type.async = true;
+  asset.type.defer = true;
+
+  t.true(asset.template.path.endsWith("/templates/async-defer.tmpl"));
+});
+
+test("default async template", t => {
+  const asset = new Asset("name");
+  asset.type.async = true;
+
+  t.true(asset.template.path.endsWith("/templates/async.tmpl"));
+});
+
+test("default defer template", t => {
+  const asset = new Asset("name");
+  asset.type.defer = true;
+
+  t.true(asset.template.path.endsWith("/templates/defer.tmpl"));
+});
+
+test("default inline template", t => {
+  const asset = new Asset("name");
+  asset.type.defer = true;
+
+  t.true(asset.template.path.endsWith("/templates/defer.tmpl"));
+});
+
+test("ensure value to replace", t => {
+  const asset = new Asset("name");
+  const error = t.throws(
+    () => {
+      asset.template.replace = 1;
+    },
+    Error
+  );
+
+  t.is(error.message, "Specify value in template to replace (as string)");
+});
+
+test("default replacement is updated", t => {
+  const asset = new Asset("name");
+  asset.template.replace = "find this";
+
+  t.is(asset.template._replace, "find this");
+  t.is(asset.template.replace, "find this");
+});
+
+test("default replacement", t => {
+  const asset = new Asset("name");
+
+  t.is(asset.template.replace, "##URL##");
+});
+
+test("default async replacement", t => {
+  const asset = new Asset("name");
+  asset.type.async = true;
+
+  t.is(asset.template.replace, "##URL##");
+});
+
+test("default defer replacement", t => {
+  const asset = new Asset("name");
+  asset.type.defer = true;
+
+  t.is(asset.template.replace, "##URL##");
+});
+
+test("default inline replacement", t => {
+  const asset = new Asset("name");
+  asset.type.inline = true;
+
+  t.is(asset.template.replace, "##SOURCE##");
+});
