d43a60804ee53a3453159955aea6f89a0aa8772c
Jouni Kantola <jouni@kantola.se>
2017-04-10 22:24:25 +0200

    Example build to sub-folder

---
+++ example/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const webpack = require("webpack");
+const ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+
+const TemplatedAssetWebpackPlugin = require("../plugins/templated-asset-webpack-plugin");
+const templatedAssetsConfig = require("./templated-assets-config.js");
+
+module.exports = {
+  entry: {
+    vendor: ["babel-polyfill", "is-thirteen"],
+    app: path.join(__dirname, "app.js")
+  },
+  resolve: {
+    modules: ["node_modules"]
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    //publicPath: "https://test-cdn.com/assets",
+    filename: "[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["env", { modules: false }]],
+            plugins: ["syntax-dynamic-import"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ["vendor", "manifest"],
+      minChunks: Infinity
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new ChunkManifestPlugin(),
+    new TemplatedAssetWebpackPlugin(templatedAssetsConfig)
+  ]
+};
