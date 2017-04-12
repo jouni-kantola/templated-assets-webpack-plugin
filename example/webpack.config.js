14a194ec60645fae4f290c9d203a0965f4364f44
Jouni Kantola <jouni.kantola@dn.se>
2017-04-12 09:00:01 +0200

    Pass asset to custom source processor

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, "dist"),
-    //publicPath: "https://test-cdn.com/assets",
+    publicPath: "https://test-cdn.com/assets",
     filename: "[name].[chunkhash].js"
   },
   module: {
