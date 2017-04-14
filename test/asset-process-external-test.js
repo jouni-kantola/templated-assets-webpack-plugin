a806252fe8ad65e1324c3387bf7548b9d31dc440
Jouni Kantola <jouni@kantola.se>
2017-04-14 15:50:39 +0200

    Notify not emit custom processed asset

--- test/asset-process-external-test.js
+++ test/asset-process-external-test.js
@@ -7,3 +7,59 @@ test("can access process external handler", t => {
 
   t.is(typeof asset.processExternal, "function");
 });
+
+test("should pass args to custom source processor", async t => {
+  const name = "a-name";
+  const asset = new Asset(name, { content: "source", filename: "file.js" });
+  const ruleArgs = ["a", "b", "c"];
+
+  asset.source.args = ruleArgs;
+
+  asset.template.process = (source, callback, ...args) => {
+    callback(ruleArgs.join("-"));
+  };
+
+  const result = await asset.process();
+
+  const expected = "a-b-c";
+  t.is(result.filename, `${name}.html`);
+  t.is(result.source, expected);
+  t.is(result.size, expected.length);
+  t.is(result.emitAsset, true);
+});
+
+test("should notify to not emit asset", async t => {
+  const name = "a-name";
+  const asset = new Asset(name, { content: "source", filename: "file.js" });
+
+  asset.output.emitAsset = false;
+  asset.template.process = (source, callback) => {
+    callback("source modified");
+  };
+
+  const result = await asset.process();
+
+  const expected = "source modified";
+  t.is(result.filename, `${name}.html`);
+  t.is(result.source, expected);
+  t.is(result.size, expected.length);
+  t.is(result.emitAsset, false);
+});
+
+test("should reject if custom source processor fails", async t => {
+  const name = "a-name";
+  const asset = new Asset(name, { content: "source", filename: "file.js" });
+
+  asset.output.emitAsset = false;
+  asset.template.process = (source, callback) => {
+    throw new Error("custom source processor failed");
+  };
+
+  try {
+    await asset.process();
+  } catch (error) {
+    const regex = new RegExp(`Templating unsuccessful for ${name}.html`);
+    t.regex(error, regex);
+    t.regex(error, /Error: custom source processor failed/);
+  }
+});
