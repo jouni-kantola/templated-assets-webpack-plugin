9e5db32730dac79d1a971e74515c752341ef3998
Jouni Kantola <jouni@kantola.se>
2017-04-14 21:02:00 +0200

    Output path format

--- test/asset-output-test.js
+++ test/asset-output-test.js
@@ -8,11 +8,35 @@ test("default to no custom output location", t => {
   t.is(asset.output.path, "");
 });
 
+test("output as string", t => {
+  const name = "a-name";
+  const asset = new Asset(name, { content: "a source", filename: "file.js" });
+
+  const error = t.throws(
+    () => {
+      asset.output.path = 1;
+    },
+    TypeError
+  );
+
+  t.is(error.message, "Specify path to template (as string)");
+});
+
 test("overrideable output path", t => {
   const name = "a-name";
   const asset = new Asset(name, { content: "a source", filename: "file.js" });
 
   asset.output.path = "a/path";
+
+  t.is(asset.output._path, "a/path");
+  t.is(asset.output.path, "a/path");
+});
+
+test("asset ends without slash", t => {
+  const name = "a-name";
+  const asset = new Asset(name, { content: "a source", filename: "file.js" });
+
+  asset.output.path = "a/path///";
 
   t.is(asset.output._path, "a/path");
   t.is(asset.output.path, "a/path");
