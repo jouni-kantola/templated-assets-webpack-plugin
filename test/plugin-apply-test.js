b8280ef3b5079131e84d7a39167bbed316bdaf2f
Jouni Kantola <jouni@kantola.se>
2017-04-18 21:59:03 +0200

    Asset types of output

--- test/plugin-apply-test.js
+++ test/plugin-apply-test.js
@@ -1,7 +1,7 @@
 import test from "ava";
 import Plugin from "../lib/plugin";
 
-test.cb("emit assets", t => {
+test.cb("emit url asset", t => {
   const plugin = new Plugin({
     rules: [
       {
@@ -29,7 +29,195 @@ test.cb("emit assets", t => {
 
     const asset = compilation.assets["url-asset.html"];
 
-    const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}"></script>`;
+    const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}"></script>\n`;
+    t.is(asset.source(), expected);
+    t.is(asset.size(), expected.length);
+    t.end();
+  };
+
+  const compiler = {
+    plugin: (event, doPluginWork) => {
+      t.is(event, "emit");
+      doPluginWork(compilation, done);
+    }
+  };
+
+  plugin.apply(compiler);
+});
+
+test.cb("emit async asset", t => {
+  const plugin = new Plugin({
+    rules: [
+      {
+        name: "async-asset",
+        output: {
+          async: true
+        }
+      }
+    ]
+  });
+
+  const compilation = {
+    chunks: [
+      {
+        name: "async-asset",
+        files: ["async-asset.js"]
+      }
+    ],
+    assets: {
+      "async-asset.js": {
+        source: () => "contents of async-assets.js"
+      }
+    }
+  };
+
+  const done = () => {
+    t.is(Object.keys(compilation.assets).length, 2);
+
+    const asset = compilation.assets["async-asset.html"];
+
+    const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}" async="async"></script>\n`;
+    t.is(asset.source(), expected);
+    t.is(asset.size(), expected.length);
+    t.end();
+  };
+
+  const compiler = {
+    plugin: (event, doPluginWork) => {
+      t.is(event, "emit");
+      doPluginWork(compilation, done);
+    }
+  };
+
+  plugin.apply(compiler);
+});
+
+test.cb("emit deferred asset", t => {
+  const plugin = new Plugin({
+    rules: [
+      {
+        name: "defer-asset",
+        output: {
+          defer: true
+        }
+      }
+    ]
+  });
+
+  const compilation = {
+    chunks: [
+      {
+        name: "defer-asset",
+        files: ["defer-asset.js"]
+      }
+    ],
+    assets: {
+      "defer-asset.js": {
+        source: () => "contents of defer-assets.js"
+      }
+    }
+  };
+
+  const done = () => {
+    t.is(Object.keys(compilation.assets).length, 2);
+
+    const asset = compilation.assets["defer-asset.html"];
+
+    const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}" defer="defer"></script>\n`;
+    t.is(asset.source(), expected);
+    t.is(asset.size(), expected.length);
+    t.end();
+  };
+
+  const compiler = {
+    plugin: (event, doPluginWork) => {
+      t.is(event, "emit");
+      doPluginWork(compilation, done);
+    }
+  };
+
+  plugin.apply(compiler);
+});
+
+test.cb("emit async/defer asset", t => {
+  const plugin = new Plugin({
+    rules: [
+      {
+        name: "async-defer-asset",
+        output: {
+          async: true,
+          defer: true
+        }
+      }
+    ]
+  });
+
+  const compilation = {
+    chunks: [
+      {
+        name: "async-defer-asset",
+        files: ["async-defer-asset.js"]
+      }
+    ],
+    assets: {
+      "async-defer-asset.js": {
+        source: () => "contents of async-defer-assets.js"
+      }
+    }
+  };
+
+  const done = () => {
+    t.is(Object.keys(compilation.assets).length, 2);
+
+    const asset = compilation.assets["async-defer-asset.html"];
+
+    const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}" async="async" defer="defer"></script>\n`;
+    t.is(asset.source(), expected);
+    t.is(asset.size(), expected.length);
+    t.end();
+  };
+
+  const compiler = {
+    plugin: (event, doPluginWork) => {
+      t.is(event, "emit");
+      doPluginWork(compilation, done);
+    }
+  };
+
+  plugin.apply(compiler);
+});
+
+test.cb("emit inline asset", t => {
+  const plugin = new Plugin({
+    rules: [
+      {
+        name: "inline-asset",
+        output: {
+          inline: true
+        }
+      }
+    ]
+  });
+
+  const compilation = {
+    chunks: [
+      {
+        name: "inline-asset",
+        files: ["inline-asset.js"]
+      }
+    ],
+    assets: {
+      "inline-asset.js": {
+        source: () => "contents of inline-assets.js"
+      }
+    }
+  };
+
+  const done = () => {
+    t.is(Object.keys(compilation.assets).length, 2);
+    const asset = compilation.assets["inline-asset.html"];
+
+    const expected = `<script type="text/javascript">${compilation.assets["inline-asset.js"].source()}</script>\n`;
     t.is(asset.source(), expected);
     t.is(asset.size(), expected.length);
     t.end();
