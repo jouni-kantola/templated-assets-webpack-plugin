fef94f1eab651e8d5d57143d6793642c9da30f5c
Jouni Kantola <jouni@kantola.se>
2017-11-06 00:56:19 +0100

    Asset module requires asset source arg

--- test/asset-type-test.js
+++ test/asset-type-test.js
@@ -1,8 +1,11 @@
 import test from "ava";
+
 import Asset from "../lib/asset";
+import AssetSource from "../lib/asset-source";
 
 test("default type", t => {
-  const asset = new Asset("name", { content: "a source", filename: "file.js" });
+  const assetSource = new AssetSource("file.js", "a source");
+  const asset = new Asset("name", assetSource, "/");
 
   t.true(asset.type.sync);
 });
