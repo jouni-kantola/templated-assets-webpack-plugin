d1083bb773cb1fa2b855c3a5353e58bb3c174aa9
Jouni Kantola <jouni@kantola.se>
2017-11-06 00:57:23 +0100

    Check URL replacement

--- test/asset-process-template-write-test.js
+++ test/asset-process-template-write-test.js
@@ -1,20 +1,26 @@
 import test from "ava";
+
 import Asset from "../lib/asset";
+import AssetSource from "../lib/asset-source";
 import io from "../lib/file-io";
 
 test.cb("should output asset", t => {
   const outputPath = "a/path";
   const name = "a-name";
+  const filename = "file.js";
+  const url = `/${filename}`;
 
   io.read = () => Promise.resolve("mocked template ##URL##");
 
   io.write = (path, content) => {
     t.is(path, `${outputPath}/${name}.html`);
-    t.is(content, "mocked template source");
+    t.is(content, `mocked template ${url}`);
     t.end();
   };
 
-  const asset = new Asset(name, { content: "source", filename: "file.js" });
+  const assetSource = new AssetSource("file.js", "a source");
+  const asset = new Asset(name, assetSource, url);
+
   asset.output.path = outputPath;
 
   asset.process();
