1bb0d5478eac277908dae73353a5dea3a160653f
Jouni Kantola <jouni.kantola@nepa.com>
2017-11-20 17:57:32 +0100

    Option for configuring asset name

--- test/templated-assets-init-test.js
+++ test/templated-assets-init-test.js
@@ -73,14 +73,26 @@ test("map chunks", t => {
       }
     },
     {
-      name: "named-asset",
+      name: "chunk-name",
       replace: "##NAME##",
       template: {
-        path: "named-asset-path",
+        path: "chunk-name-path",
         header: "a-header",
         footer: "a-footer"
       },
       args: [1, 2, 3]
+    },
+    {
+      name: "chunk-name-to-override",
+      output: {
+        name: "overriden-chunk-name"
+      }
+    },
+    {
+      name: "another-chunk-name-to-override",
+      output: {
+        name: defaultName => defaultName.split("-")[0]
+      }
     }
   ]);
 
@@ -102,16 +114,26 @@ test("map chunks", t => {
       source: "non-match-source"
     },
     {
-      name: "named-asset",
+      name: "chunk-name",
       filename: "na.js",
       url: "/na.js",
-      source: "named-asset-source"
+      source: "chunk-name-source"
+    },
+    {
+      name: "chunk-name-to-override",
+      url: "/na.js",
+      filename: "na.js"
+    },
+    {
+      name: "another-chunk-name-to-override",
+      url: "/na.js",
+      filename: "na.js"
     }
   ];
 
   const templatedAssets = new TemplatedAssets(chunks, rules);
 
-  t.is(templatedAssets.assets.length, 3);
+  t.is(templatedAssets.assets.length, 5);
 
   const asset1 = templatedAssets.assets[0];
   t.is(asset1.file.extension, "html");
@@ -127,12 +149,19 @@ test("map chunks", t => {
   t.is(asset2.template.header, "a-header");
   t.is(asset2.template.footer, "a-footer");
   t.is(asset2.template.replace.test("##NAME##"), true);
+  t.is(asset2.file.filename, "chunk-name.html");
   t.deepEqual(asset2.args, [1, 2, 3]);
 
   const asset3 = templatedAssets.assets[2];
-  t.is(asset3.file.extension, "txt");
-  t.is(asset3.file.prefix, "__");
-  t.is(asset3.type.inline, true);
-  t.is(asset3.output.emitAsset, false);
-  t.deepEqual(asset3.output.path, ["output/path"]);
+  t.is(asset3.file.filename, "overriden-chunk-name.html");
+
+  const asset4 = templatedAssets.assets[3];
+  t.is(asset4.file.filename, "another.html");
+
+  const asset5 = templatedAssets.assets[4];
+  t.is(asset5.file.extension, "txt");
+  t.is(asset5.file.prefix, "__");
+  t.is(asset5.type.inline, true);
+  t.is(asset5.output.emitAsset, false);
+  t.deepEqual(asset5.output.path, ["output/path"]);
 });
