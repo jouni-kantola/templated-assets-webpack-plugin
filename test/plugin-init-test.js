88c625f1242f081edbd27e70e3a4114f3265da75
Jouni Kantola <jouni@kantola.se>
2020-09-11 14:02:17 +0200

    Default to rule for matching all JS/CSS files

--- test/plugin-init-test.js
+++ test/plugin-init-test.js
@@ -1,13 +1,18 @@
 import test from "ava";
 import Plugin from "../lib/plugin";
 
-test("fallback to empty rule set", t => {
+test("fallback to rule set for all JavaScript and CSS files", (t) => {
   const { rules } = new Plugin().pluginOptions;
   t.true(Array.isArray(rules));
-  t.is(rules.length, 0);
+  t.is(rules.length, 1);
+  const rule = rules.pop();
+
+  t.true(rule.test.test("a-javascript-file.abc1234.js"));
+  t.true(rule.test.test("a-css-file.abc1234.css"));
+  t.false(rule.test.test("an-svg-file.abc1234.svg"));
 });
 
-test("init rules", t => {
+test("init rules", (t) => {
   const { rules } = new Plugin({
     rules: [{ name: "an-entry" }]
   }).pluginOptions;
