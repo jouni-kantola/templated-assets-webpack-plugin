be2c1eb5d90d3797d49dbbe9a687486ce7ba4f59
Jouni Kantola <jouni.kantola@nepa.com>
2020-11-05 22:29:10 +0100

    Fixed incorrect type as asset source

--- test/asset-process-template-write-multiple.js
+++ test/asset-process-template-write-multiple.js
@@ -1,30 +1,38 @@
 import test from "ava";
 import Asset from "../lib/asset";
+import AssetSource from "../lib/asset-source";
 import io from "../lib/file-io";
 
 test.cb("should process multiple copies", t => {
   t.plan(4);
 
+  const name = "a-name";
+  const filename = "file.js";
+  const url = `/${filename}`;
+
   io.read = () => Promise.resolve("mocked template ##URL##");
 
+  const expectedContent = `mocked template ${url}`;
+  const outputPath1 = "a/path";
+  const outputPath2 = "another/path";
   const output1 = (path, content) => {
-    t.is(path, "a/path/a-name.html");
-    t.is(content, "mocked template source");
+    t.is(path, `${outputPath1}/${name}.html`);
+    t.is(content, expectedContent);
   };
-
   const output2 = (path, content) => {
-    t.is(path, "another/path/a-name.html");
-    t.is(content, "mocked template source");
+    t.is(path, `${outputPath2}/${name}.html`);
+    t.is(content, expectedContent);
     t.end();
   };
 
   io.write = (path, content) => {
-    if (path.startsWith("a/path")) output1(path, content);
-    if (path.startsWith("another/path")) output2(path, content);
+    if (path.startsWith(outputPath1)) output1(path, content);
+    if (path.startsWith(outputPath2)) output2(path, content);
   };
 
-  const asset = new Asset("a-name", { content: "source", filename: "file.js" });
-  asset.output.path = ["a/path", "another/path"];
+  const assetSource = new AssetSource(filename, "source");
+  const asset = new Asset("a-name", assetSource, url);
+  asset.output.path = [outputPath1, outputPath2];
 
   asset.process();
 });
